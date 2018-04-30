41906c7f95a978511c7bb3279f0acebb6ee09e4a
Jun.Nakajima <ikuta1919@gmail.com>
2018-04-30 20:28:46 +0900

    【add】Form1、VendingLogic

---
+++ VendingMachine/VendingMachine/VendingLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    /// <summary>
+    /// 自動販売機のインターフェース
+    /// </summary>
+    interface IVending
+    {
+        /// <summary>
+        /// お金を入金する
+        /// </summary>
+        /// <param name="amount"></param>
+        void PutMoney(int amount);
+        
+        /// <summary>
+        /// 飲み物を買う
+        /// </summary>
+        /// <param name=""></param>
+        void BuyDrink(int )
+
+    }
+
+
+    /// <summary>
+    /// 自動販売機のロジック
+    /// </summary>
+    public class VendingLogic
+    {
+
+
+    }
+}
